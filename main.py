6e74059c2f8995261b303e26867a9bf7f056e822
etonkou <85410307+etonkou@users.noreply.github.com>
2023-09-29 20:33:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ import plotly.express as px
 # load dataset
 data = pd.read_csv('data/california_housing_train.csv')
 
-print('*'*500)
+print('*'*50)
 
 print(data)
 
 # tris sur les colonnes utiles pour notre analyse
 data = data[['total_rooms','total_bedrooms','population','households']]
 
-print('*'*500)
+print('*'*50)
 print('tris sur les colonnes utiles pour notre analyse')
 print(data)
 
 # Verification des valeurs manquantes
-print('*'*500)
+print('*'*50)
 print('Verification des valeurs manquantes')
 d = data.isnull().sum() 
 print(d)
 
 # Verification des valeurs doubles
-print('*'*500)
+print('*'*50)
 data.duplicated().sum()
 print('Verification des valeurs doubles')
 d = data.duplicated().sum() 
@@ -33,18 +33,25 @@ print(d)
 
 
 # Analyse bivariee
-print('*'*500)
+print('*'*50)
 print("Analyse bivariee")
 sb.scatterplot(data['total_rooms'],data['total_bedrooms'])
 sb.scatterplot(data['households'],data['total_bedrooms']) 
 sb.scatterplot(data['households'],data['population']) 
 
-print('*'*500)
-# Matrice de correlation
-cor_matrice = data.corr()
-print(cor_matrice)
+# print('*'*50)
+# # Matrice de correlation
+# cor_matrice = data.corr()
+# print(cor_matrice)
 
-print('*'*500)
+print('*'*50)
+print('Suppression les valeurs extremes')
+index = data[(data['total_bedrooms'] >= 430)|(data['total_bedrooms'] <= 420)].index
+data.drop(index, inplace=True)
+d = data['total_bedrooms'].describe()
+print(d)
+
+print('*'*50)
 print("Traitement des valeurs extremes")
 continuous_features = data.select_dtypes('float64').columns
 for col in continuous_features:
